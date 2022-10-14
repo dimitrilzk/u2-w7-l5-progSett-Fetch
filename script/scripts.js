b0d891e5c46bf73075cb348944dced60c5d39e05
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-14 13:01:54 +0200

    1

--- script/scripts.js
+++ script/scripts.js
@@ -3,7 +3,7 @@ async function visualizeAllBooks() {
   let textBooks = await books.json();
   console.log(textBooks);
   for (let allBooks of textBooks) {
-    document.querySelector(".container>.row").innerHTML += `<div class="card" style="width: 18rem;">
+    document.querySelector(".container>.row").innerHTML += `<div class="card" style="width: 20rem;">
     <img src="${allBooks.img}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${allBooks.title}</h5>
