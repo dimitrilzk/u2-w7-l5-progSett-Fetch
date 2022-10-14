36269d160dc26928f3f14f0b233db334846f46f9
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-14 12:54:41 +0200

    quasi renderizzato

--- script/scripts.js
+++ script/scripts.js
@@ -1,11 +1,19 @@
 async function visualizeAllBooks() {
   let books = await fetch("https://striveschool-api.herokuapp.com/books");
   let textBooks = await books.json();
-  //   console.log(textBooks);
+  console.log(textBooks);
   for (let allBooks of textBooks) {
-    document.getElementById("libri").innerHTML += `<p>${allBooks.title}</p>`;
+    document.querySelector(".container>.row").innerHTML += `<div class="card" style="width: 18rem;">
+    <img src="${allBooks.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${allBooks.title}</h5>
+      <p class="card-text">${allBooks.category}</p>
+      <a href="#" class="btn btn-primary">${allBooks.price}</a>
+    </div>
+  </div>`;
   }
 }
 window.onload = () => {
   visualizeAllBooks();
 };
+/* <p>${allBooks.title}</p> */
