a8d5be5cff59f38c51872c7bf8918cc9c22024b0
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-14 12:28:57 +0200

    installato bootstrap

--- script/scripts.js
+++ script/scripts.js
@@ -1,7 +1,7 @@
 async function visualizeAllBooks() {
   let books = await fetch("https://striveschool-api.herokuapp.com/books");
   let textBooks = await books.json();
-  console.log(textBooks);
+  //   console.log(textBooks);
   for (let allBooks of textBooks) {
     document.getElementById("libri").innerHTML += `<p>${allBooks.title}</p>`;
   }
