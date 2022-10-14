1d78df4df37e98c635fb4469e715bc971f4a4206
dimitrilzk <dimitrilazuka95@gmail.com>
2022-10-14 11:54:31 +0200

    fatto rimane da abbellire

--- script/scripts.js
+++ script/scripts.js
@@ -1,14 +1,10 @@
-// let allBooks = fetch("https://striveschool-api.herokuapp.com/books").then((risultato) => {
-//   console.log(risultato);
-//   risultato.text().then((risultatoTesto) => {
-//     console.log(risultatoTesto);
-//     console.log(JSON.parse(risultatoTesto));
-//   });
-// });
 async function visualizeAllBooks() {
   let books = await fetch("https://striveschool-api.herokuapp.com/books");
   let textBooks = await books.json();
   console.log(textBooks);
+  for (let allBooks of textBooks) {
+    document.getElementById("libri").innerHTML += `<p>${allBooks.title}</p>`;
+  }
 }
 window.onload = () => {
   visualizeAllBooks();
